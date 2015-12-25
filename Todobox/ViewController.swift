2eea54160ac1318e71fceb30285b4269e27ee4f2
Suyeol Jeon <devxoul@gmail.com>
2015-12-26 02:10:16 +0900

    Xcode 프로젝트 시작

---
+++ Todobox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todobox
+//
+//  Created by 전수열 on 12/26/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
